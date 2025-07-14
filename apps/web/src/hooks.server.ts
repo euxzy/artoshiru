1bd14ab5a0ff14f5ab1b29a6feb6419552c0ab4f
euxzy <mhd.rijal203@gmail.com>
2025-07-14 22:42:44 +0700

    feat(web): create middleware for authenticated route

---
+++ apps/web/src/hooks.server.ts
@@ -0,0 +1,26 @@
+import { authClient } from '$lib/auth-client'
+import { redirect, type Handle } from '@sveltejs/kit'
+
+const PRIVATE_PREFIX = ['/dashboard']
+const AUTH = '/auth'
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const pathname = event.url.pathname
+  if (PRIVATE_PREFIX.some((prefix) => pathname.startsWith(prefix)) || pathname.startsWith(AUTH)) {
+    const { data: session } = await authClient.getSession({
+      fetchOptions: {
+        headers: event.request.headers,
+      },
+    })
+
+    if (!session?.session && !pathname.startsWith(AUTH)) {
+      return redirect(302, `${AUTH}/login`)
+    }
+
+    if (session?.session && pathname.startsWith(AUTH)) {
+      return redirect(302, '/dashboard')
+    }
+  }
+
+  return await resolve(event)
+}

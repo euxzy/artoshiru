50438929555ad8e107c830b65c1650041ab2b43e
euxzy <mhd.rijal203@gmail.com>
2025-07-15 13:23:41 +0700

    feat(auth): add usernameClient plugin for better-auth

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { PUBLIC_SERVER_URL } from '$env/static/public'
+import { usernameClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/svelte'
 
 export const authClient = createAuthClient({
   baseURL: PUBLIC_SERVER_URL,
+  plugins: [usernameClient()],
 })

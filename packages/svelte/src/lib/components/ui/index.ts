38747fe525ffbca84217286429d3db91c79d84f0
euxzy <mhd.rijal203@gmail.com>
2025-07-17 15:23:56 +0700

    feat(ui): add shadcn components

--- packages/svelte/src/lib/components/ui/index.ts
+++ packages/svelte/src/lib/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './button/index.js'
 export * from './card/index.js'
 export * from './input/index.js'
+export * from './label/index.js'
 export * from './separator/index.js'
 export * from './skeleton/index.js'
 export * from './tooltip/index.js'

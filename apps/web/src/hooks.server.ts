57d6821e4459e0a7bcc3301b213a73488fc81851
euxzy <mhd.rijal203@gmail.com>
2025-07-22 20:54:49 +0700

    chore: change sveltekit adapter

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,4 +1,3 @@
-import { env } from '$env/dynamic/public'
 import { authClient } from '$lib/auth-client'
 import { redirect, type Handle } from '@sveltejs/kit'
 
@@ -8,21 +7,13 @@ const AUTH = '/auth'
 export const handle: Handle = async ({ event, resolve }) => {
   const pathname = event.url.pathname
 
-  // TODO: should be remove all console after debug
-
   if (PRIVATE_PREFIX.some((prefix) => pathname.startsWith(prefix)) || pathname.startsWith(AUTH)) {
-    console.log('server base url', env.PUBLIC_SERVER_URL)
-
     const { data: session } = await authClient.getSession({
       fetchOptions: {
-        baseURL: env.PUBLIC_SERVER_URL,
         headers: event.request.headers,
         credentials: 'include',
-        mode: 'cors', // try to add cors mode
       },
     })
-
-    console.log('session', session)
 
     if (!session?.session && !pathname.startsWith(AUTH)) {
       return redirect(302, `${AUTH}/login`)
